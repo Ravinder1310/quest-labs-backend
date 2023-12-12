1dd067faa3a63f24ee2574aa282fc1beb260d3b1
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-12-12 20:30:48 +0530

    some chnges in the model of articles

--- models/ArticlesModel.js
+++ models/ArticlesModel.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 
 const articlesSchema = new mongoose.Schema({
-    photo:{
-        type:String,
-        required:true
-    },
     tabs:{
         type:String,
         required:true
